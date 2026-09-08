fcef2ec67286ab84461e42f2865a74f213728d63
Effo12345 <4p7septipuses@gmail.com>
2026-09-07 21:58:28 -0400

    Move runner away from python

--- Main.py
+++
@@ -1,9 +0,0 @@
-import sys
-import subprocess
-
-def main():
-  res = subprocess.run(["cargo", "run", sys.argv[1]], capture_output=True, text=True)
-  print(res.stdout, end='')
-
-if __name__ == "__main__":
-  main()
